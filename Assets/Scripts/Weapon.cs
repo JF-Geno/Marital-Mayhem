47be29a5d54928874296ac4ea43a51a7e264dac7
Tyson Robinson <TyRobinson4@shaw.ca>
2024-05-10 10:30:17 -0500

    projectile movement

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public Transform firePoint;
+
+    void Update()
+    {
+        //if(input)
+    }
+
+}
