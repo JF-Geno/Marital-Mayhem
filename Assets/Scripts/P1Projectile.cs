e983b60346611fd429f709b577bbb8dd06b05080
Tyson Robinson <TyRobinson4@shaw.ca>
2024-05-13 09:47:15 -0500

    finished projectiles

--- Assets/Scripts/P1Projectile.cs
+++ Assets/Scripts/P1Projectile.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class P1Projectile : MonoBehaviour
 {
     public float speed = 20f;
-    public int damage = 3;
+    public int damage = 4;
     public Rigidbody2D rb;
+    public float degreesPerSec = 360f;
 
     void Start()
     {
-        rb.velocity = transform.right * speed;         
+        rb.velocity = transform.right * speed;
+    }
+
+    void Update()
+    {
+        float rotAmount = degreesPerSec * Time.deltaTime;
+        float curRot = transform.localRotation.eulerAngles.z;
+        transform.localRotation = Quaternion.Euler(new Vector3(0, 0, curRot + rotAmount));
     }
 
     void OnTriggerEnter2D (Collider2D hitInfo)
--- Assets/Scripts/P2Projectile.cs
+++ Assets/Scripts/P2Projectile.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class P2Projectile : MonoBehaviour
 {
     public float speed = 20f;
-    public int damage = 3;
+    public int damage = 4;
     public Rigidbody2D rb;
+    public float degreesPerSec = 360f;
+
     void Start()
     {
-        rb.velocity = transform.right * speed;         
+        rb.velocity = transform.right * speed;
     }
 
-    void OnTriggerEnter2D (Collider2D hitInfo)
+    void Update()
+    {
+        float rotAmount = degreesPerSec * Time.deltaTime;
+        float curRot = transform.localRotation.eulerAngles.z;
+        transform.localRotation = Quaternion.Euler(new Vector3(0, 0, curRot + rotAmount));
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
     {
         P1Health p1Health = hitInfo.GetComponent<P1Health>();
         if (p1Health != null)
