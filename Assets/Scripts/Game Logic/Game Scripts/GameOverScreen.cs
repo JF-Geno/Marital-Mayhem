71e6b5d23ae14d2bf8c83c3e6e7243b542205942
Tyson Robinson <TyRobinson4@shaw.ca>
2024-06-04 11:26:53 -0500

    menu button fix

--- Assets/Scripts/Game Logic/Game Scripts/GameOverScreen.cs
+++ Assets/Scripts/Game Logic/Game Scripts/GameOverScreen.cs
@@ -8,15 +8,9 @@ using static System.Net.Mime.MediaTypeNames;
 
 public class GameOverScreen : MonoBehaviour
 {
-    public TextMeshProUGUI winText;
-    public TextMeshProUGUI winStatsText;
-    public GameObject music;
-    public GameObject youSuck;
-    public float timeLeft = 3.0f;
     public void Setup(string playerName)
     {
         gameObject.SetActive(true);
-        winText.text = playerName + " WINS";
         if (playerName == GameValues.player1Name)
         {
             GameValues.player1Wins++;
@@ -25,13 +19,11 @@ public class GameOverScreen : MonoBehaviour
         {
             GameValues.player2Wins++;
         }
-        winStatsText.text = $"{GameValues.player1Name}: {GameValues.player1Wins}\t{GameValues.player2Name}: {GameValues.player2Wins}";
 
-        music.SetActive(false);
-        youSuck.SetActive(true);  
     }
     public void MainMenuButton()
     {
+        Debug.Log("Working");
         SceneManager.LoadScene(0);
     }
 }
