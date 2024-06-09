670449cef853babbb98f18233d03fdff12067b2e
Tyson Robinson <TyRobinson4@shaw.ca>
2024-06-09 11:52:02 -0500

    Character controls fix

--- Assets/Backgrounds/Background.cs
+++ Assets/Backgrounds/Background.cs
@@ -15,13 +15,14 @@ public class Background : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Player Player1 = players.First(p => p.playerName == GameValues.player1Name);
-        Player Player2 = players.First(p => p.playerName == GameValues.player2Name);
         BackgroundToChange.GetComponent<SpriteRenderer>().sprite = GameValues.Map;
-        Player1.playerNumControl = 1;
-        Player2.playerNumControl = 2;
 
+        Player Player1 = players.First(p => p.playerName == GameValues.player1Name);
+        Player1.playerNumControl = 1;
         Player newObject1 = Instantiate(Player1, p1.transform.position, p1.transform.rotation);
+
+        Player Player2 = players.First(p => p.playerName == GameValues.player2Name);
+        Player2.playerNumControl = 2;
         Player newObject2 = Instantiate(Player2, p2.transform.position, p2.transform.rotation);
 
         //Player newObject = Instantiate(p1, PlayerOneCharacter.transform.position, PlayerOneCharacter.transform.rotation);
