2aa10d1015ca9a17bd073b823f3d2d5cf4c3f2e5
JF-Geno <obondejfgeno@gmail.com>
2024-06-09 18:43:32 -0500

    change in AttackArea

--- Assets/Scripts/Player Logic/PlayerScriptLogic/AttackArea.cs
+++ Assets/Scripts/Player Logic/PlayerScriptLogic/AttackArea.cs
@@ -13,7 +13,7 @@ public class AttackArea : MonoBehaviour
       var player = collider.GetComponent<Player>();
         if (player != null)
         {
-            //KnockBack(collider);
+            KnockBackFunction(collider);
             collider.GetComponent<Bill_Man>().Damage(damage, GameValues.DamageTypes.Melee);
             collider.gameObject.GetComponent<Bill_Man>().UltimateLogic();
             punchNoise.SetActive(false);
@@ -27,57 +27,8 @@ public class AttackArea : MonoBehaviour
         KnockBack knockBack = collider.GetComponent<KnockBack>();
         if (knockBack != null)
         {
-            //KnockBack(collider);
-            collider.GetComponent<Sarah_Woman>().Damage(damage, GameValues.DamageTypes.Melee);
-            collider.gameObject.GetComponent<Sarah_Woman>().UltimateLogic();
-            punchNoise.SetActive(false);
-            punchNoise.SetActive(true);
+            bool knockFromRight = collider.transform.position.x <= transform.position.x;
+            knockBack.ApplyKnockBack(knockFromRight);
         }
-
-        //David_Brother
-        if (collider.GetComponent<David_Brother>() != null)
-        {
-            //KnockBack(collider);
-            collider.GetComponent<David_Brother>().Damage(damage, GameValues.DamageTypes.Melee);
-            collider.gameObject.GetComponent<David_Brother>().UltimateLogic();
-            punchNoise.SetActive(false);
-            punchNoise.SetActive(true);
-
-        }
-
-        //Jessica_Babysitter
-        if (collider.GetComponent<Jessica_Babysitter>() != null)
-        {
-            //KnockBack(collider);
-            collider.GetComponent<Jessica_Babysitter>().Damage(damage, GameValues.DamageTypes.Melee);
-            collider.gameObject.GetComponent<Jessica_Babysitter>().UltimateLogic();
-            punchNoise.SetActive(false);
-            punchNoise.SetActive(true);
-
-        }
-
-
-        //Kathy_CatLady
-        if (collider.GetComponent<Kathy_CatLady>() != null)
-        {
-            //KnockBack(collider);
-            collider.GetComponent<Kathy_CatLady>().Damage(damage, GameValues.DamageTypes.Melee);
-            collider.gameObject.GetComponent<Kathy_CatLady>().UltimateLogic();
-            punchNoise.SetActive(false);
-            punchNoise.SetActive(true);
-
-        }
-
-        //Saul_Lawyer
-        if (collider.GetComponent<Saul_Lawyer>() != null)
-        {
-            //KnockBack(collider);
-            collider.GetComponent<Saul_Lawyer>().Damage(damage, GameValues.DamageTypes.Melee);
-            collider.gameObject.GetComponent<Saul_Lawyer>().UltimateLogic();
-            punchNoise.SetActive(false);
-            punchNoise.SetActive(true);
-
-        }
-
     }
 }
